2bf105d231960ad435e1eab320095c77b00db4a8
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2025-11-04 20:06:47 +0500

    added

---
+++ src/component/HeroSec/OurTrendyProducts/Card2.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import img from '../../../assets/images/for-small-desk-ai- plant-2.png'
+import { IoBagHandleOutline } from "react-icons/io5";
+
+const Card2 = () => {
+  return (
+    <div className='bg-white/5 backdrop-blur-md px-6 pb-5  rounded-[100px] shadow-lg border border-white/10 w-6xl mx-auto my-10 '>
+      <figure className='grid grid-cols-2'>
+
+        
+        <figcaption className='text-white flex flex-col justify-center text-left gap-2 ps-20'>
+            <h1 className='text-[22px] font-medium'>For Small Decs Ai Plant</h1>
+            <p className='text-[14px] font-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+            <span className='text-[18px] font-medium'>Rs. 599/-</span>
+            
+            <div className='flex flex-row items-center justify-start gap-2'>
+              <button className='border rounded-[4px] px-7 font-light text-[14px] py-1'>Explore</button>
+              <button className='border px-2 py-[0.40rem] rounded-[4px]'><IoBagHandleOutline /></button>
+            </div>
+            
+            </figcaption>
+            <div className='-mt-10 ps-30'><img src={img} alt="" className='w-60'/></div>
+          
+      </figure>
+    </div>
+  )
+}
+
+export default Card2
--- src/component/HeroSec/OurTrendyProducts/OurTrendyProduct.jsx
+++ src/component/HeroSec/OurTrendyProducts/OurTrendyProduct.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card from './Card'
+import Card2 from './Card2'
 
 const OurTrendyProduct = () => {
   return (
-    <div>
+    <div className='flex flex-col gap-10'>
       <Card />
+      <Card2 />
     </div>
   )
 }

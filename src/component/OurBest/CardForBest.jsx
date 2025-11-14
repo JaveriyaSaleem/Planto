6e48479d7fcca0bde71b1629cb4dcc37b715dddf
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2025-11-14 06:20:20 +0500

    added

--- src/component/OurBest/CardForBest.jsx
+++ src/component/OurBest/CardForBest.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import img from "../../assets/images/our-best-2.png";
+import flower2 from "../../assets/images/our-best-1.png";
+import flower3 from "../../assets/images/desk-plant-top-selling-cropped.png";
+import flower4 from "../../assets/images/Calat-O2-plant-cropped.png";
 import { IoBagHandleOutline } from "react-icons/io5";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
-const CardForBest = () => {
+const CardForBest = (props) => {
   return (
-    <div className="bg-white/5 backdrop-blur-md px-6 pb-5  rounded-[100px] shadow-lg border border-white/10 w-6xl mx-auto my-10 ">
+    <div className="bg-white/5 backdrop-blur-md mt-30 rounded-[100px] shadow-lg border border-white/10 w-6xl py-2 mx-auto my-10 ">
       <figure className="grid grid-cols-2">
-        <div className="-mt-50">
-          <img src={img} alt="" className="" />
+        <div className="-mt-40">
+          <img src={props.img} alt="" className="w-full" />
         </div>
-        <figcaption className="text-white flex flex-col text-left gap-2 pt-20 pe-30">
+        <figcaption className="text-white flex flex-col text-left gap-4 pt-20 pe-30 ">
           <h1 className="text-[22px] font-medium">
             We Have Small And Best O2 Plants Collection’s
           </h1>
@@ -22,13 +27,17 @@ const CardForBest = () => {
           </p>
           {/* <span className="text-[18px] font-medium">Rs. 599/-</span> */}
 
-          <div className="flex flex-row items-center justify-start gap-2">
-            <button className="border rounded-[4px] px-7 font-light text-[14px] py-1">
+          <div className="flex flex-row items-center justify-between gap-2">
+            <button className="border rounded-lg px-10 font-light text-[14px] py-1">
               Explore
             </button>
-            <div className="">
+            <div className="flex flex-row items-end">
 
-            &lt;      &gt;
+            <IoIosArrowBack className="text-[20px]"/>
+
+            <p className="flex flex-row items-end"><span className="text-[14px]">0{props.number}/</span><span className="text-[11px]">04</span></p>
+            <IoIosArrowForward className="text-[20px]"/>
+            
             </div>
           </div>
         </figcaption>
--- src/component/OurBest/OurBest2.jsx
+++ src/component/OurBest/OurBest2.jsx
@@ -1,15 +1,95 @@
 import React from 'react'
 import LCorneredBorder from '../HeroSec/LCorneredBorder'
 import CardForBest from './CardForBest'
+import flower1 from "../../assets/images/our-best-2-edited.png";
+import flower2 from "../../assets/images/ourbest1.png";
+import flower3 from "../../assets/images/Untitled_design-removebg-preview.png";
+import flower4 from "../../assets/images/Calat-O2-plant-cropped.png";
+// Swiper imports
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
 
 const OurBest2 = () => {
+  const cardOfOurBest =[
+      {
+    imgName: flower1,
+    number: 1
+  },
+        {
+    imgName: flower2,
+    number: 2
+  },
+        {
+    imgName: flower3,
+    number: 3
+  },
+        {
+    imgName: flower4,
+    number: 4
+  },
+
+];   
   return (
     <div>
 
        <div className='text-center'>
            <LCorneredBorder title={"Our Best o2"}/>
        </div>
-       <CardForBest/>
+
+      <Swiper
+  modules={[Autoplay, Pagination]}
+  autoplay={{
+    delay: 3000,
+    disableOnInteraction: false,
+  }}
+  spaceBetween={20}
+  slidesPerView={'auto'}
+  centeredSlides={true}
+  loop={true}
+  pagination={{ clickable: true }}
+  breakpoints={{
+    768: {
+      slidesPerView: 1,
+      centeredSlides: false,
+    },
+    1024: {
+      slidesPerView: 1,
+      centeredSlides: false,
+    },
+  }}
+>
+          {cardOfOurBest.map(({imgName, number}, index) => (
+           <SwiperSlide key={index} className=''>
+  <CardForBest img={imgName} number={number}/>
+</SwiperSlide>
+          ))}
+          
+
+        </Swiper>
+
+      <style>{`
+        .swiper-pagination {
+          margin-top: 40px !important;
+          position: relative !important;
+
+        }
+            .swiper-pagination-bullet {
+    background-color: white !important; /* Inactive dots */
+    opacity: 0.3;
+    box-shadow: 0 2px 5px #ffff;
+  border-radius: 50%;
+  }
+
+  .swiper-pagination-bullet-active {
+    background-color: white !important; /* Active dot */
+    opacity: 1;
+    transform: scale(1.2);
+  }
+      `}</style>
+      
     </div>
   )
 }
